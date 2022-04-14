f05c8b212aea717d3d7e76d0ade6d901122dc1da
RonaldoYusuf <90240652+ronaldoyusuf@users.noreply.github.com>
2022-04-14 15:41:38 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Ronaldo Yusuf, 51019018, Sistem Informasi\n')
 })
 
 app.listen(PORT, HOST)
